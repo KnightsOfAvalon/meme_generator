c8cc0eb8be8cc9be67eb55e732f270dbea492200
Ava Matthew <washington_ava@yahoo.com>
2020-10-31 10:50:35 -0500

    Updates

---
+++ precache-manifest.739ffb749d50e3608bc3e06be74269f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d260537e5ae635b517676597319b6e3",
+    "url": "/meme_generator/index.html"
+  },
+  {
+    "revision": "997a00dc3b618dd892bb",
+    "url": "/meme_generator/static/css/main.c6288bf6.chunk.css"
+  },
+  {
+    "revision": "e60d95a5e10aa0ef22e5",
+    "url": "/meme_generator/static/js/2.9b39589c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/meme_generator/static/js/2.9b39589c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997a00dc3b618dd892bb",
+    "url": "/meme_generator/static/js/main.6418d74b.chunk.js"
+  },
+  {
+    "revision": "0616506a0aca983d3f44",
+    "url": "/meme_generator/static/js/runtime-main.1ab436de.js"
+  }
+]);
